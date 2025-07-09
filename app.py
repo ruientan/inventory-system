bf929c1668d40714142e773edd3f730ab0c80f5c
ruientan <e0968059@u.nus.edu>
2025-07-09 16:42:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1637,5 +1637,5 @@ def dashboard_metrics():
 
 # ─────────────────── Run the server ───────────────────
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
