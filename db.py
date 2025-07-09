fb0af53165f4726144b1a93aa008f2c6e541c2f6
ruientan <e0968059@u.nus.edu>
2025-07-09 16:45:08 +0800

    Modified db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import mysql.connector
 def get_connection():
     return mysql.connector.connect(
         host=os.environ.get("DB_HOST"),
-        port=os.environ.get("DB_PORT", 3306),
+        port=int(os.environ.get("DB_PORT", 3306)),
         user=os.environ.get("DB_USER"),
         password=os.environ.get("DB_PASSWORD"),
         database=os.environ.get("DB_NAME")
